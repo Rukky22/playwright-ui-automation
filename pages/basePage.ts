e31b67cd19c7de4768f8a0ede35ffb907e34dfec
Randolph Rukevwe Oghwe <rnkkis@gmail.com>
2025-11-23 16:45:50 +0100

    Modified BasePage code

---
+++ pages/basePage.ts
@@ -0,0 +1,52 @@
+import { expect, Page } from "@playwright/test";
+
+export default class BasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async waitForPageLoad() {
+    await this.page.waitForLoadState("networkidle");
+  }
+
+  async navigateTo(path: string) {
+    await this.page.goto(path);
+  }
+
+  async getTitle() {
+    return this.page.title();
+  }
+
+  async getCurrentURL() {
+    return this.page.url();
+  }
+
+  async clickElement(selector: string) {
+    await this.page.click(selector);
+  }
+
+  async enterText(locator: string, text: string) {
+    await this.page.fill(locator, text);
+  }
+
+  async isElementVisible(locator: string) {
+    return this.page.locator(locator).isVisible();
+  }
+
+  async getElementText(locator: string) {
+    return this.page.locator(locator).innerText();
+  }
+
+  async takeScreenshot(name: string) {
+    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    await this.page.screenshot({
+      path: `screenshots/${name}-${timestamp}.png`,
+    });
+  }
+
+  async scrollToTop() {
+    await this.page.evaluate(() => window.scrollTo(0, 0));
+  }
+}
